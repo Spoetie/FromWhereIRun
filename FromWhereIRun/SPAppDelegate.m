20137fe315709033fd4276fba5c152d77ba9ce1c
Leah <leah.culver@gmail.com>
2013-11-07 17:10:22 -0800

    login / logout

--- FromWhereIRun/SPAppDelegate.m
+++ FromWhereIRun/SPAppDelegate.m
@@ -10,22 +10,67 @@
 
 #import "SPAppDelegate.h"
 #import "SPAuthViewController.h"
+#import "SPRunListViewController.h"
+
+@interface SPAppDelegate ()
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+@property (strong, nonatomic) SPAuthViewController *authViewController;
+
+@end
 
 @implementation SPAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    DBAccountManager* accountMgr = [[DBAccountManager alloc] initWithAppKey:@"3vd1skts4bpm221" secret:@"1r0gjas376qzg4g"];
-    [DBAccountManager setSharedManager:accountMgr];
-    
-    SPAuthViewController *viewController = [[SPAuthViewController alloc] initWithNibName:@"SPAuthViewController" bundle:nil];
+    SPRunListViewController *runListViewController = [[SPRunListViewController alloc] initWithNibName:@"SPRunListViewController" bundle:nil];
 
-    self.window.rootViewController = viewController;
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:runListViewController];
+
+    self.window.rootViewController = self.navigationController;
     [self.window makeKeyAndVisible];
+
+    DBAccountManager *accountManager = [[DBAccountManager alloc] initWithAppKey:@"3vd1skts4bpm221" secret:@"1r0gjas376qzg4g"];
+    [DBAccountManager setSharedManager:accountManager];
+
+    self.authViewController = [[SPAuthViewController alloc] initWithNibName:@"SPAuthViewController" bundle:nil];
+
+    DBAccount *account = [[DBAccountManager sharedManager] linkedAccount];
+    if (account == nil) {
+        [self.navigationController presentViewController:self.authViewController animated:NO completion:nil];
+    }
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(logout)
+                                                 name:SPLogoutNotification
+                                               object:nil];
 
     return YES;
 }
-							
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    DBAccount *account = [[DBAccountManager sharedManager] handleOpenURL:url];
+    
+    if (account) {
+        [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+        return YES;
+    }
+
+    return NO;
+}
+
+- (void)logout
+{
+    DBAccount *account = [[DBAccountManager sharedManager] linkedAccount];
+    if (account) {
+        [account unlink];
+    }
+
+    // Show auth screen
+    [self.navigationController presentViewController:self.authViewController animated:YES completion:nil];
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -51,6 +96,12 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)dealloc
+{
+    // Unregister for all notifications
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
---
+++ FromWhereIRun/SPRunListViewController.h
@@ -0,0 +1,15 @@
+//
+//  SPRunListViewController.h
+//  FromWhereIRun
+//
+//  Created by Leah Culver on 11/7/13.
+//  Copyright (c) 2013 Spoetie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSString * const SPLogoutNotification;
+
+@interface SPRunListViewController : UITableViewController
+
+@end
